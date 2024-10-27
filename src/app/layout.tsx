6b3e8721483d33961caf510c033ea85db697c278
Gauravst <gauravsinghtangariya1@gmail.com>
2024-10-28 00:30:39 +0530

    auth done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import { Manrope, DM_Sans } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import { ThemeProvider } from '@/components/theme';
 
 const manrope = DM_Sans({ subsets: ['latin'] });
@@ -18,12 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
         <body className={`${manrope.className} bg-[#171717]`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
+            forcedTheme="dark"
             disableTransitionOnChange
           >
             {children}
